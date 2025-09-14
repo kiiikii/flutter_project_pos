977062623d4862803653f2a2bb3281f508692eaa
refky setiawan <shiejacko@gmail.com>
2025-09-14 19:30:44 +0700

    adding transaction_page, modified file admin_dashboard & cashier_dashboard

--- lib/views/admins/admin_dashboard.dart
+++ lib/views/admins/admin_dashboard.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import '../../widgets/ra_drawer.dart';
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/views/cashiers/cashier_dashboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'transaction_page.dart';
+// import 'expense_page.dart';
+// import '../../shared/';
+
+class CashierDashboard extends StatelessWidget {
+  const CashierDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
